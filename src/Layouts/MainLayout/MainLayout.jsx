5797907ee642ff4011ae7eca5f3612f8bbae7500
Menna ayman <mennaaymangalal4@gmail.com>
2025-04-11 15:30:04 +0200

    update

--- src/Layouts/MainLayout/MainLayout.jsx
+++ src/Layouts/MainLayout/MainLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import { Outlet } from 'react-router-dom'
+import img from '../../assets/wraper.png'
 
 export default function MainLayout() {
   return (
     <>
+    <div className="">
+     <img src={img} className='w-full h-44'/>
+    </div>
       <Navbar/>
       <div className="container">
         <Outlet/>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <h1>Home</h1>
-      <Button color="primary">Button</Button>
+     
     </>
   )
 }
